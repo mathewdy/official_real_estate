cb0fc2313337805484257ea7b7ad9e7e0b129475
Raie01 <ryantecling@gmail.com>
2022-11-13 17:07:47 +0800

    Update Home.php

--- Users/Home.php
+++ Users/Home.php
@@ -82,12 +82,39 @@ $start_page = ($page-1)*05;
                     <label for="">Email:</label>
                     <p><?php echo $row_users['email']?></p>
                     <!---modal for edit button--->
-                    <a href="edit-profile.php?user_id=<?php echo $row_users['user_id']?>">
+                    <a href="edit-profile.php?user_id=<?php echo $row_users['user_id']?>" data-bs-toggle="modal" data-bs-target="#users">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                         </svg>
                     </a>
+                    <div class="modal fade" id="users" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <form action="" method="POST">
+                            <div class="modal-body">
+                                <label for="">First Name:</label>
+                                <input type="text" name="first_name" class="form-control" value="<?php echo $row_users['first_name']?>" required>
+                                <label for="">Middle Name:</label>
+                                <input type="text" name="middle_name" class="form-control" value="<?php echo $row_users['middle_name']?>" required>
+                                <label for="">Last Name:</label>
+                                <input type="text" name="last_name" class="form-control" value="<?php echo $row_users['last_name']?>" required>
+                                <label for="">Contact Number:</label>
+                                <input type="text" name="contact_number" class="form-control" value="<?php echo $row_users['contact_number']?>" required>
+                                <label for="">Email:</label>
+                                <input type="text" name="email" class="form-control" value="<?php echo $row_users['email']?>">
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <input type="submit" class="btn btn-primary" name="update_profile" value="Save Changes">
+                            </div>
+                            </form>
+                            </div>
+                        </div>
+                    </div>
 
                 <?php
             }
